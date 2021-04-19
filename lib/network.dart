cefd1a75340963db98e8c311c5dd9e830d8cb97a
andrsem <andriisem0@gmail.com>
2021-04-19 13:14:18 +0300

    create abstract classes

--- lib/network.dart
+++ lib/network.dart
@@ -20,14 +20,16 @@ class NewsModel {
 const _url = 'https://hacker-news.firebaseio.com/v0';
 
 // create api provider (hacker news api example)
-class NewsApiProvider {
+class NewsApiProvider implements Source {
   Client client = Client();
+  @override
   Future<List<int>> fetchTopId() async {
     final response = await client.get(Uri.parse('$_url/topstories.json'));
     final ids = json.decode(response.body) as List;
     return ids.cast<int>();
   }
 
+  @override
   Future<NewsModel> fetchItemFromId(int id) async {
     final response = await client.get(Uri.parse('$_url/item$id'));
     final parsedJson = json.decode(response.body) as Map<String, dynamic>;
@@ -35,12 +37,21 @@ class NewsApiProvider {
   }
 }
 
-// repository class which decides if data is in DB if not fetch and store to DB
+// create this abstract class to be able to connect different providers to repository
+abstract class Source {
+  Future<List<int>> fetchTopId();
+  Future<NewsModel> fetchItemFromId(int id);
+}
+
+// cache data to DB
+abstract class Cache {
+  void adItem();
+}
+
+// repository class if data is in DB use it if not fetch and store data to DB
 class Repository {
   NewsApiProvider apiProvider = NewsApiProvider();
-  Future fetchTopIds() {
-    return apiProvider.fetchTopId();
-  }
+  Future fetchTopIds() => apiProvider.fetchTopId();
 
   Future<NewsModel> fetchItem(int id) async {
     // check for item in DB if yes return item
