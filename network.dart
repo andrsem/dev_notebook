c8e9d8296abf2533a326c0bbefaea55bc058914b
andrsem <andriisem0@gmail.com>
2021-04-19 08:25:39 +0300

    create model class

--- network.dart
+++ network.dart
@@ -1,7 +1,18 @@
 // ----------Working with JSON------------
-// 
-// by Stephen Grinder
-// 
+//
+//           by Stephen Grinder
+//
 // 1. make HTTP request
 // 2. decode JSON
 // 3. put JSON into a model class
+//
+
+class Model {
+  final String name;
+  final int age;
+
+// named constructor
+  Model.fromJson(Map<String, dynamic> parsedJson)
+      : name = parsedJson['name'],
+        age = parsedJson['age'];
+}
