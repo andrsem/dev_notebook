88563fd62d5d8325fc0be442751b3819797442ff
andrsem <andriisem0@gmail.com>
2021-04-21 09:06:35 +0300

    create dart tips

---
+++ lib/flutter&dart/dart_tips.dart
@@ -0,0 +1,10 @@
+// spread operator in collections adds elements of collection in new collection, it is like collection extracts its values and then add them to new collection
+void main() {
+  print(colors);
+}
+
+final colors = <String>[
+  'grey',
+  'blue',
+  ...['red', 'brown'],
+];
