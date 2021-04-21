64925df11bd5e869e8bbea537f4fb12a404b878a
andrsem <andriisem0@gmail.com>
2021-04-21 09:15:42 +0300

    if collections and spread operator in maps

--- lib/flutter&dart/dart_tips.dart
+++ lib/flutter&dart/dart_tips.dart
@@ -3,8 +3,19 @@ void main() {
   print(colors);
 }
 
-final colors = <String>[
+final colors = [
   'grey',
   'blue',
   ...['red', 'brown'],
 ];
+
+// here use if collection to add some fields using spread operator if certain condition is met
+const ratings = [4.0, 4.5, 3.5];
+final restaurant = <String, dynamic>{
+  'name': 'Pizza Buena',
+  'cuisine': 'Spanish',
+  if (ratings.length > 3) ...<String, dynamic>{
+    'rating': ratings,
+    'isPopular': true,
+  },
+};
