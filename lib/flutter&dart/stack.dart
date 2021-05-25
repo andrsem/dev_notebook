4ee08f74786302455c802e15cb21a9e33fffafc2
andrsem <andriisem0@gmail.com>
2021-05-25 14:24:51 +0300

    create simple stack using composition

---
+++ lib/flutter&dart/stack.dart
@@ -0,0 +1,10 @@
+class Stack<T> {
+  final List<T> _items = [];
+  void push(T item) => _items.add(item);
+  T pop() => _items.removeLast();
+}
+
+void main() {
+  final Stack stack = Stack<int>()..push(3)..push(4)..push(6);
+  print(stack.pop());
+}
