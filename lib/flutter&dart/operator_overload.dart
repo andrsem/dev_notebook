395041e3cfc261164d475395ae611ed383bacd9d
andrsem <andriisem0@gmail.com>
2021-05-25 14:17:45 +0300

    use equatable

--- lib/flutter&dart/operator_overload.dart
+++ lib/flutter&dart/operator_overload.dart
@@ -1,15 +1,9 @@
-class Point {
+import 'package:equatable/equatable.dart';
+
+class Point extends Equatable {
   const Point(this.x, this.y);
   final int x;
   final int y;
-
-  @override
-  String toString() => 'Point($x, $y)';
-
-  @override
-  bool operator ==(covariant Point other) {
-    return x == other.x && y == other.y;
-  }
 
   @override
   Point operator +(covariant Point add) {
@@ -20,12 +14,18 @@ class Point {
   Point operator *(covariant int multiply) {
     return Point(x * multiply, y * multiply);
   }
+
+  @override
+  List<Object> get props => [x, y];
+  @override
+  bool get stringify => true;
 }
 
 void main() {
   // make this compile by overriding the + operator
-  print(Point(1, 1) + Point(2, 0)); // should print: Point(3, 1)
+  print(Point(1, 1) == Point(1, 1)); // should print: Point(3, 1)
   // make this compile by overriding the * operator
   print(Point(2, 1) * 5); // should print: Point(10, 5)
   print(Point(2, 1) * 5);
+  print(Point(1, 2));
 }
